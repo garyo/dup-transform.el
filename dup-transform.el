6cbfb9b6fdf811a4e1e2ddad993fb16f35ec7622
Gary Oberbrunner <garyo@darkstarsystems.com>
2024-09-22 15:24:50 -0400

    Fix commentary to fit into 80 char lines

--- dup-transform.el
+++ dup-transform.el
@@ -24,8 +24,10 @@
 
 ;;; Commentary:
 
-;; A package for graphics coders, to duplicate a line or region, handling rgb(a), xyz(w), [0]-[2] etc.
-;; To use, place point on a line to be duplicated & transformed. That line should have a pattern
+;; A package for graphics coders, to duplicate a line or region,
+;; handling rgb(a), xyz(w), [0]-[2] etc.
+;; To use, place point on a line to be duplicated & transformed.
+;; That line should have a pattern
 ;; like the following:
 ;; * .x (will be transformed to .y .z .w)
 ;; * ->x (similar)
@@ -33,7 +35,8 @@
 ;; * ->r (similar)
 ;; * red (to green blue alpha)
 ;; * [0] (to [1] [2] [3])
-;; Use C-u (repeat count) to set total number of repeats. Defaults to 3 if the line/region contains rgb-type text,
+;; Use C-u (repeat count) to set total number of repeats.
+;; Defaults to 3 if the line/region contains rgb-type text,
 ;; otherwise 2.
 
 ;;; Code:
